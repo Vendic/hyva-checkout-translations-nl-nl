519384c04311994c7777bac42a9a07b62a6e37aa
Tjitse Efdé <tjitse@vendic.nl>
2024-08-19 13:44:51 +0200

    Fix for nl_BE translations

--- src/nl_BE/registration.php
+++ src/nl_BE/registration.php
@@ -4,6 +4,6 @@ use Magento\Framework\Component\ComponentRegistrar;
 
 ComponentRegistrar::register(
     ComponentRegistrar::LANGUAGE,
-    'vendic_hyva-checkout-nl',
+    'vendic_hyva-checkout-nl-be',
     __DIR__
 );
